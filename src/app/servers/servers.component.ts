6aecae5f7c77f987b276666a5b7d45771d47998c
Orlando Flores Silva <orlandofury@gmail.com>
2023-01-12 23:19:10 -0600

    Property Binding you link a DOM property to a variable

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,11 +4,21 @@ import { Component } from '@angular/core';
   //selector: '[app-servers]',
   // selector: '.app-servers',
   selector: 'app-servers',
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>`,
+  templateUrl: './servers.component.html',
+  // template: `
+  // <app-server></app-server>
+  // <app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
-
+  allowNewServer = false;
+  /**
+   *
+   */
+  constructor() {
+    setTimeout(()=>{
+      this.allowNewServer = true;
+    },2000);
+    
+  }
 }
