1fb90bb3090566eaa4d35d0605f6be8670c4a2a1
Orlando Flores Silva <orlandofury@gmail.com>
2023-01-13 00:00:38 -0600

    Combining all forms of databinding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -25,7 +25,7 @@ export class ServersComponent {
   }
 
   onCreateServer(){
-    this.serverCreationStatus = 'Server was created!';
+    this.serverCreationStatus = 'Server was created! Name is '+ this.serverName;
   }
   onUpdateServerName(event: Event){
     this.serverName = (<HTMLInputElement>event.target).value;
