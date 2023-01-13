226741a325310de926394e9965505f4ef6cfc436
Orlando Flores Silva <orlandofury@gmail.com>
2023-01-12 23:48:08 -0600

    Passing and Using Data with Event Binding:  is used to pass the reference of the event which contains info of the target an input element

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -13,6 +13,7 @@ import { Component } from '@angular/core';
 export class ServersComponent {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
+  serverName = '';
   /**
    *
    */
@@ -25,5 +26,8 @@ export class ServersComponent {
 
   onCreateServer(){
     this.serverCreationStatus = 'Server was created!';
+  }
+  onUpdateServerName(event: Event){
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 }
