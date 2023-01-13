61aa323b429d9b1cae41ea21cf1b2e3518cb8b4c
Orlando Flores Silva <orlandofury@gmail.com>
2023-01-13 02:01:23 -0600

    Directives *ngif and else #condition

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -14,9 +14,8 @@ export class ServersComponent {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = 'TestServer';
-  /**
-   *
-   */
+  serverCreated = false;
+
   constructor() {
     setTimeout(()=>{
       this.allowNewServer = true;
@@ -25,6 +24,7 @@ export class ServersComponent {
   }
 
   onCreateServer(){
+    this.serverCreated = true;
     this.serverCreationStatus = 'Server was created! Name is '+ this.serverName;
   }
   onUpdateServerName(event: Event){
