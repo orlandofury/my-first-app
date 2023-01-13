62ce70ef6aeda9f2cd93b28fa20e00aed3c8a714
Orlando Flores Silva <orlandofury@gmail.com>
2023-01-13 01:37:44 -0600

    Adding solution for assignment 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  //styleUrls: ['./app.component.css']
-  styles: [`
-    h3 {
-      color: dodgerblue
-    }
-  `]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  username = '';
 }
