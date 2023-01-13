8c6636c2adcabc453372cc2b955a36ec4f0daacb
Orlando Flores Silva <orlandofury@gmail.com>
2023-01-13 01:22:43 -0600

    Cleaning assignment 1

--- src/app/success-alert/success-alert.component.ts
+++ src/app/success-alert/success-alert.component.ts
@@ -1,7 +1,7 @@
 import { Component } from "@angular/core";
 
 @Component({
-    selector:'success-alert',
+    selector:'app-success-alert',
     templateUrl:'./success-alert.component.html',
     styleUrls: ['./success-alert.component.css']
 })
