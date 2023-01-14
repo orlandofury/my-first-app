229dc0eb8b8b38a6451b49151ddb1671328a9bac
Orlando Flores Silva <orlandofury@gmail.com>
2023-01-14 13:20:43 -0600

    Adding solution for assignment 3: log version working with any content not only numbers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   log =[];
   onToggleDisplay(){
     this.showSecret = !this.showSecret;
-    this.log.push(this.log.length + 1);
+    //this.log.push(this.log.length + 1);
+    this.log.push(new Date());
   }
 }
