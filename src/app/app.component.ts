2d41156421e8f9d3dd613108768bb574ae6553e6
Orlando Flores Silva <orlandofury@gmail.com>
2023-01-14 13:18:32 -0600

    Adding solution for assignment 3: version with only loggin numbers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  //styleUrls: ['./app.component.css']
-  styles: [`
-    h3 {
-      color: dodgerblue
-    }
-  `]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  showSecret = false;
+  log =[];
+  onToggleDisplay(){
+    this.showSecret = !this.showSecret;
+    this.log.push(this.log.length + 1);
+  }
 }
